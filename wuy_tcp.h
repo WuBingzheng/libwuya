2499f7e179f0825a856cbacad99d56a6f4240b12
wub <wubingzheng@gmail.com>
2018-08-22 16:46:38 +0800

    better tcp

--- wuy_tcp.h
+++ wuy_tcp.h
@@ -11,6 +11,7 @@
 
 #include <sys/types.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -21,34 +22,36 @@
 #include <errno.h>
 
 
-static inline int tcp_set_sndbuf(int fd, int value)
+static inline int wuy_tcp_set_sndbuf(int fd, int value)
 {
 	return setsockopt(fd, SOL_SOCKET, SO_SNDBUF, &value, sizeof(int));
 }
-static inline int tcp_get_sndbuf(int fd)
+static inline int wuy_tcp_get_sndbuf(int fd)
 {
-	int value, size = 4;
+	int value, size = sizeof(int);
 	getsockopt(fd, SOL_SOCKET, SO_SNDBUF, &value, &size);
 	return value;
 }
-static inline int tcp_get_rcvbuf(int fd)
-{
-	int value, size = 4;
-	getsockopt(fd, SOL_SOCKET, SO_RCVBUF, &value, &size);
-	return value;
-}
-
-static inline int tcp_set_rcvbuf(int fd, int value)
+static inline int wuy_tcp_set_rcvbuf(int fd, int value)
 {
 	return setsockopt(fd, SOL_SOCKET, SO_RCVBUF, &value, sizeof(int));
 }
-
-static inline int tcp_set_cork(int fd, int cork)
+static inline int wuy_tcp_get_rcvbuf(int fd)
+{
+	int value, size = sizeof(int);
+	getsockopt(fd, SOL_SOCKET, SO_RCVBUF, &value, &size);
+	return value;
+}
+static inline int wuy_tcp_set_cork(int fd, int cork)
 {
 	return setsockopt(fd, IPPROTO_TCP, TCP_CORK, &cork, sizeof(int));
 }
+static inline int wuy_tcp_set_defer_accept(int fd, int value)
+{
+	return setsockopt(fd, IPPROTO_TCP, TCP_DEFER_ACCEPT, &value, sizeof(int));
+}
 
-static inline int wuy_tcp_listen(struct sockaddr *sa, int x)
+static inline int wuy_tcp_listen(struct sockaddr *sa, int backlog, bool reuse_port)
 {
 	int fd = socket(sa->sa_family, SOCK_STREAM, 0);  
 	if (fd < 0) {
@@ -56,19 +59,14 @@ static inline int wuy_tcp_listen(struct sockaddr *sa, int x)
 	}
 
 	if (fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK) != 0) {
+		close(fd);
 		return -1;
 	}
 
-	int val = 1;
-	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(int)) < 0) {
-		return -1;
-	}
-	if (setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &val, sizeof(int)) < 0) {
-		return -1;
-	}
-
-	val = 60;
-	if (setsockopt(fd, IPPROTO_TCP, TCP_DEFER_ACCEPT, &val, sizeof(int)) < 0) {
+	int value = 1;
+	int opt = reuse_port ? SO_REUSEPORT : SO_REUSEADDR;
+	if (setsockopt(fd, SOL_SOCKET, opt, &value, sizeof(int)) < 0) {
+		close(fd);
 		return -1;
 	}
 
@@ -77,25 +75,15 @@ static inline int wuy_tcp_listen(struct sockaddr *sa, int x)
 		return -1;
 	}
 
-	listen(fd, 1000);
+	listen(fd, backlog);
 	return fd;
 }
 
 
 static inline int wuy_tcp_accept(int listen_fd, struct sockaddr *client_addr)
 {
-	int client_fd;
 	socklen_t addrlen = sizeof(struct sockaddr);
-again:
-	// client_fd = accept4(listen_fd, client_addr, &addrlen, SOCK_NONBLOCK);
-	client_fd = accept(listen_fd, client_addr, &addrlen);
-	if (client_fd < 0) {
-		if (errno == EINTR)
-			goto again;
-		return client_fd;
-	}
-	int ret = fcntl(client_fd, F_SETFL, fcntl(client_fd, F_GETFL) | O_NONBLOCK); // XXX
-	return client_fd;
+	return accept4(listen_fd, client_addr, &addrlen, SOCK_NONBLOCK);
 }
 
 static inline int wuy_tcp_connect(struct sockaddr *sa)
@@ -114,7 +102,6 @@ static inline int wuy_tcp_connect(struct sockaddr *sa)
 	int ret = connect(fd, sa, sizeof(struct sockaddr));
 	if (ret < 0 && errno != EINPROGRESS) {
 		close(fd);
-		printf("close 4\n");
 		return -1;
 	}
 
