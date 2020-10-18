2c76b931896c6ffa12d7d7efbba910551f89ddee
wub <wubingzheng@gmail.com>
2020-10-18 16:01:51 +0800

    add wuy_rand_double()

--- wuy_rand.h
+++ wuy_rand.h
@@ -5,6 +5,12 @@ static inline long wuy_rand_range(long range)
 {
 	return random() / (RAND_MAX / range + 1);
 }
+
+static inline double wuy_rand_double(void)
+{
+	return (double)random() / ((double)RAND_MAX + 1);
+}
+
 static inline bool wuy_rand_sample(double rate)
 {
 	return random() < RAND_MAX * rate;
