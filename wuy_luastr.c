4e6cf1ee210cf15b335718ebc043137501447cf5
wub <wubingzheng@gmail.com>
2021-02-28 19:07:12 +0800

    fix wuy_luastr

--- wuy_luastr.c
+++ wuy_luastr.c
@@ -1,6 +1,7 @@
 #include <lua5.1/lua.h>
 #include <lua5.1/lauxlib.h>
 #include <lua5.1/lualib.h>
+#include <assert.h>
 
 #include "wuy_luastr.h"
 
@@ -8,59 +9,54 @@ static lua_State *wuy_luastr_L = NULL;
 
 static void wuy_luastr_init(void)
 {
-	printf("wuy_luastr_init\n");
 	if (wuy_luastr_L == NULL) {
-		return;
+		wuy_luastr_L = lua_open();
+		luaL_openlibs(wuy_luastr_L);
+		lua_getglobal(wuy_luastr_L, "string");
 	}
-	wuy_luastr_L = lua_open();
-	luaL_openlibs(wuy_luastr_L);
-	printf(" 1\n");
-	lua_getglobal(wuy_luastr_L, "string");
-	printf(" 2\n");
 }
 
 const char *wuy_luastr_gsub(const char *s, const char *pattern, const char *repl)
 {
 	wuy_luastr_init();
 
-	printf("before %d\n", lua_gettop(wuy_luastr_L));
 	lua_getfield(wuy_luastr_L, -1, "gsub");
-	printf("after %d\n", lua_gettop(wuy_luastr_L));
 
+	/* arguments */
 	lua_pushstring(wuy_luastr_L, s);
 	lua_pushstring(wuy_luastr_L, pattern);
 	lua_pushstring(wuy_luastr_L, repl);
-	if (lua_pcall(wuy_luastr_L, 3, 2, 0) != 0){
-		return NULL;
-	}
 
+	/* run */
+	assert(lua_pcall(wuy_luastr_L, 3, 2, 0) == 0);
+
+	/* 2 return values */
 	const char *out = lua_tostring(wuy_luastr_L, -2);
 	int n = lua_tointeger(wuy_luastr_L, -1);
 
-	/* 2 return values and 1 function */
-	lua_pop(wuy_luastr_L, 3);
+	lua_pop(wuy_luastr_L, 2);
 
 	return n != 0 ? out : NULL;
 }
 
-bool wuy_luastr_find(const char *s, const char *pattern)
+int wuy_luastr_find(const char *s, const char *pattern, int *p_end)
 {
 	wuy_luastr_init();
 
-	printf("before %d\n", lua_gettop(wuy_luastr_L));
 	lua_getfield(wuy_luastr_L, -1, "find");
-	printf("after %d\n", lua_gettop(wuy_luastr_L));
 
+	/* arguments */
 	lua_pushstring(wuy_luastr_L, s);
 	lua_pushstring(wuy_luastr_L, pattern);
-	if (lua_pcall(wuy_luastr_L, 2, 2, 0) != 0){
-		return NULL;
-	}
 
-	bool found = lua_isnumber(wuy_luastr_L, -1);
+	/* run */
+	assert(lua_pcall(wuy_luastr_L, 2, 2, 0) == 0);
 
-	/* 2 return values and 1 function */
-	lua_pop(wuy_luastr_L, 3);
+	/* 2 return values */
+	int start = lua_tonumber(wuy_luastr_L, -2) - 1;
+	*p_end = lua_tonumber(wuy_luastr_L, -1) - 1;
 
-	return found;
+	lua_pop(wuy_luastr_L, 2);
+
+	return start;
 }
--- wuy_luastr.h
+++ wuy_luastr.h
@@ -5,6 +5,13 @@
 
 const char *wuy_luastr_gsub(const char *s, const char *pattern, const char *repl);
 
-bool wuy_luastr_find(const char *s, const char *pattern);
+int wuy_luastr_find(const char *s, const char *pattern, int *p_end);
+
+static inline bool wuy_luastr_find2(const char *s, const char *pattern)
+{
+	int end;
+	int start = wuy_luastr_find(s, pattern, &end);
+	return start >= 0;
+}
 
 #endif
